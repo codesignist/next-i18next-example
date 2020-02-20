6ef45485daa082e7d2ce15476dd8357df03ddcf9
Yunus Eş <yunuses@ihh.org.tr>
2020-02-20 14:04:04 +0300

    Next config settings changed

--- i18n.js
+++ i18n.js
@@ -4,20 +4,12 @@
 */
 
 const NextI18Next = require("next-i18next").default;
-const { localeSubpaths } = require("next/config").default().publicRuntimeConfig;
-
-const localeSubpathVariations = {
-  none: {},
-  foreign: {
-    de: "de"
-  },
-  all: {
-    en: "en",
-    de: "de"
-  }
-};
 
 module.exports = new NextI18Next({
-  otherLanguages: ["de"],
-  localeSubpaths: localeSubpathVariations[localeSubpaths]
+  defaultLanguage: "tr",
+  otherLanguages: ["en", "de"],
+  localeSubpaths: {
+    en: "en",
+    ar: "de"
+  }
 });
